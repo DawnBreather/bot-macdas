ef4928805203e89797c8f1b97a289c0e0649ed58
Ilya Gruzd <i5558804@gmail.com>
2021-03-12 23:16:24 +0300

    delete mistake

--- main.py
+++ main.py
@@ -73,7 +73,6 @@ def protocol_new(last_state):
 
 def entrypoint():
     last_state = State(db_mode=DbMode.DYNAMODB)
-    send_new_posts(datetime.fromtimestamp(last_state.time))
     if last_state.time:
         if last_state.time == (last_candle(last_state.main_period) - (last_state.main_period * 2 * 60)):
             protocol_update(last_state)
