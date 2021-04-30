9e0f2a34f49ea88c97e1a8729c97bbcffee4e127
Ilya Gruzd <i5558804@gmail.com>
2021-04-30 20:39:28 +0300

    comment str

--- main.py
+++ main.py
@@ -187,5 +187,5 @@ def lambda_handler(event=None, context=None):
     entrypoint()
 
 
-last_state = State(db_mode=DbMode.DYNAMODB)
-protocol_new(last_state)
+# last_state = State(db_mode=DbMode.DYNAMODB)
+# protocol_new(last_state)
